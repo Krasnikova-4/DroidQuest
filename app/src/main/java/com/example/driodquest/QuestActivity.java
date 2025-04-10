e617329734b6030d18246cff92b9f407826ec776
Ulia758 <uliakolosova15@gmail.com>
2025-04-10 13:41:44 +0300

    Android и модель MVC

--- app/src/main/java/com/example/driodquest/QuestActivity.java
+++ app/src/main/java/com/example/driodquest/QuestActivity.java
@@ -3,6 +3,7 @@ package com.example.driodquest;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -14,7 +15,32 @@ import androidx.core.view.WindowInsetsCompat;
 public class QuestActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
 
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_android, true),
+            new Question(R.string.question_linear, false),
+            new Question(R.string.question_service, false),
+            new Question(R.string.question_res, true),
+            new Question(R.string.question_manifest, true),
+    };
+    private int mCurrentIndex = 0;
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue =
+                mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResId = 0;
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,21 +51,36 @@ public class QuestActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+            mQuestionTextView =
+                    (TextView) findViewById(R.id.question_text_view);
+
+
+
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuestActivity.this, R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+               checkAnswer(true);
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuestActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
+
 
     }
 
